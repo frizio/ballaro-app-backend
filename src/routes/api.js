6b782e691950981af58e65aa88503553467ce9d6
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-09-19 17:25:17 +0200

    Log the /testdb route

--- src/routes/api.js
+++ src/routes/api.js
@@ -13,7 +13,7 @@ const pool = new Pool(
 router.get(
     '/',
     (req, res) => {
-        console.log("Get route /");
+        console.log("Hit route /testdb with GET");
         res.json( { name: 'Hello from Ballaro backend' } );
     }
 );
@@ -22,15 +22,18 @@ router.get(
 router.get(
     '/testdb', 
     async (req, res) => {
+        console.log("Hit route /testdb with GET");
         try {
-        const client = await pool.connect()
-        const result = await client.query('SELECT * FROM test_table');
-        const results = { 'results': (result) ? result.rows : null};
-        res.json(results);
-        client.release();
+            console.log("Connect to the database. Request is: ");
+            console.log(req);
+            const client = await pool.connect()
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'info': (result) ? result.rows : null};
+            res.json(results);
+            client.release();
         } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
+            console.error(err);
+            res.send("Error " + err);
         }
     }
 );
