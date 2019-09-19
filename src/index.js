3106ed8d6c2262444a53d89c1daa64b8c0d3ce3a
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-09-19 17:59:03 +0200

    Test for serving static file manually

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const api = require('./routes/api');
+const path = require('path');
 
 // Initialization and configs
 const app = express();
@@ -16,6 +17,40 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/api', api);
+
+// Web pages 
+app.get(
+    '/',
+    (req, res) => {
+        console.log('Home page hit');
+        res.sendFile(path.join(__dirname, '/views/index.html'));
+    }
+);
+
+// Data Files
+app.get(
+    '/data/fruttadistagione',
+    (req, res) => {
+        console.log('Data hit');
+        res.sendFile(path.join(__dirname, '/data/stagionalita_frutta.json'));
+    }
+);
+
+app.get(
+    '/data/verduradistagione',
+    (req, res) => {
+        console.log('Data hit');
+        res.sendFile(path.join(__dirname, '/data/stagionalita_verdura.json'));
+    }
+);
+
+app.get(
+    '/data/pescedistagione',
+    (req, res) => {
+        console.log('Data hit');
+        res.sendFile(path.join(__dirname, '/data/stagionalita_pescato.json'));
+    }
+);
 
 // Static files
 
