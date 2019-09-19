83c9d5662bac7a9413dc5979dbd543f6cccb1797
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-09-19 17:15:35 +0200

    Add /testdb API route to test db connection

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,37 @@
 const { Router } = require('express');
+const { Pool } = require('pg');
+
 const router = Router();
+
+const pool = new Pool(
+    {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    }
+);
 
 router.get(
     '/',
     (req, res) => {
         console.log("Get route /");
         res.json( { name: 'Hello from Ballaro backend' } );
+    }
+);
+
+
+router.get(
+    '/testdb', 
+    async (req, res) => {
+        try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.json(results);
+        client.release();
+        } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+        }
     }
 );
 
