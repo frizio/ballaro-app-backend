4661d00b7dc92bd09311203be1a4a8724f94e516
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-10-03 20:01:08 +0200

    Fix query string and add /negozi route

--- src/routes/api.js
+++ src/routes/api.js
@@ -25,9 +25,28 @@ router.get(
         console.log("Hit route /testdb with GET");
         try {
             console.log("Connect to the database. Request is: ");
-            console.log(req);
+            //console.log(req);
             const client = await pool.connect()
             const result = await client.query('SELECT * FROM test_table');
+            const results = { 'info': (result) ? result.rows : null};
+            res.json(results);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    }
+);
+
+router.get(
+    '/negozi', 
+    async (req, res) => {
+        console.log("Hit route /negozi with GET");
+        try {
+            console.log("Connect to the database");
+            const client = await pool.connect()
+            const query_string = 'SELECT * FROM negozi';
+            const result = await client.query(query_string);
             const results = { 'info': (result) ? result.rows : null};
             res.json(results);
             client.release();
@@ -46,8 +65,10 @@ router.get(
         const provincia = req.params.provincia;
         try {
             console.log("Connect to the database");
+            const query_string = `SELECT * FROM negozi where provincia = \'${provincia}\'`;
+            console.log(query_string);
             const client = await pool.connect()
-            const result = await client.query(`SELECT * FROM negozi where provincia = "${provincia}"`);
+            const result = await client.query(query_string);
             const results = { 'info': (result) ? result.rows : null};
             res.json(results);
             client.release();
